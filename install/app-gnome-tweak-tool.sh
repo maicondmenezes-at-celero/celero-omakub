0619cb09cfdd5a809f2335d678f1f2a6267d33ee
Maicon de Menezes <maicondmenezes@gmail.com>
2024-06-24 15:01:32 -0300

    UPDATE: change app-gnome-tweak-tool to add debian 12 support

--- install/app-gnome-tweak-tool.sh
+++ install/app-gnome-tweak-tool.sh
@@ -1,1 +1,12 @@
-sudo apt install -y gnome-tweak-tool
+# Check linux distro
+
+if [ "$DISTRO" == "ubuntu" ]; then
+    # install for ubuntu
+
+    sudo apt install -y gnome-tweak-tool
+
+elif [ "$DISTRO" == "debian" ]; then
+    # install for debian
+
+    sudo apt install -y gnome-tweaks
+
