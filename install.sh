c3b31ef55c3257694ee87abf3ad0266abc156604
Maicon de Menezes <maicondmenezes@gmail.com>
2024-06-24 12:27:24 -0300

    ADD: detect linux distro for choose the rigth method to install and set each feature

--- install.sh
+++ install.sh
@@ -1,6 +1,18 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
+# Determine the OS and set the environment variable
+if [ -f /etc/debian_version ]; then
+    export DISTRO="debian"
+    echo "Debian-based OS detected"
+elif [ -f /etc/lsb-release ]; then
+    export DISTRO="ubuntu"
+    echo "Ubuntu-based OS detected"
+else
+    echo "Unsupported OS"
+    exit 1
+fi
+
 # Needed for all installers
 sudo apt update -y
 sudo apt install -y curl git unzip
@@ -18,6 +30,9 @@ sudo apt upgrade -y
 # Revert to normal idle and lock settings
 gsettings set org.gnome.desktop.screensaver lock-enabled true
 gsettings set org.gnome.desktop.session idle-delay 300
+
+# Unset the DISTRO environment variable after use
+unset DISTRO
 
 # Logout to pickup changes
 gum confirm "Ready to logout for all settings to take effect?" && gnome-session-quit --logout --no-prompt
