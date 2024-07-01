aae73ef984f5653992bd188c61cc06c2bd67284d
Maicon de Menezes <maicondmenezes@gmail.com>
2024-07-01 04:35:14 -0300

    FIX: kubernetes install for debian

--- install/essential/app-kubectl.sh
+++ install/essential/app-kubectl.sh
@@ -1,5 +1,6 @@
 # Title: Kubectl
 # Description: The Kubernetes command-line tool
+# Reference: https://kubernetes.io/docs/tasks/tools/install-kubectl-linux/
 
 dearmor_gpg_key() {
     local input_file=$1
@@ -35,10 +36,12 @@ if [ "$DISTRO" == "ubuntu" ]; then
 
 elif [ "$DISTRO" == "debian" ]; then
     # Debian-specific installation steps
+    
     sudo install -m 0755 -d /etc/apt/keyrings
-    sudo curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-    sudo chmod a+r /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ $(. /etc/os-release && echo "$VERSION_CODENAME") main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+    sudo curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+    sudo chmod 644 /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+
+    echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
     sudo chmod 644 /etc/apt/sources.list.d/kubernetes.list
 fi
 
